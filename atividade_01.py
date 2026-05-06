c873c320d5b207231ff9954a42f37c5ab10144ff
Ricardo Vieira Viana <ricardviana1@gmail.com>
2026-05-05 22:44:14 -0300

    Ajuste nas atividades

--- atividade_01.py
+++ atividade_01.py
@@ -39,9 +39,10 @@ print("\nEstrutura de Vendas:")
 df_vendedores.printSchema()
 
 # Atividades
+print("Atividades")
 
 # 1) Crie uma consulta que mostre, nesta ordem, Nome, Estado e Status
-print("\nCrie uma consulta que mostre, nesta ordem, Nome, Estado e Status")
+print("\n1) Crie uma consulta que mostre, nesta ordem, Nome, Estado e Status")
 
 print("\nSolução 1:")
 colunas = ["Cliente", "Estado", "Status"]
@@ -55,11 +56,11 @@ df_clientes.select(
 ).show(5)
 
 # 2) Crie uma consulta que mostre apenas os clientes do Status "platiun" e "gold"
-print("\nCrie uma consulta que mostre apenas os clientes do Status ""platiun"" e ""gold""")
+print("\n2) Crie uma consulta que mostre apenas os clientes do Status ""platiun"" e ""gold""")
 df_clientes.filter(col("Status").isin("Platinum", "Gold")).show()
 
 # 3) Demostre quanto cada Status de Clientes representa em vendas
-print("Demostre quanto cada Status de Clientes representa em vendas")
+print("3) Demostre quanto cada Status de Clientes representa em vendas")
 df_vendas_join = df_itens_vendas.join(df_vendas, df_itens_vendas.VendasID == df_vendas.VendasID, "left")
 df_final = df_vendas_join.join(df_clientes, df_vendas_join.ClienteID == df_clientes.ClienteID, "left")
 
